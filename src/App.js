256f1c0edd808e8db9a1f9f2cc797d81a9b91d0e
Chris Ducote <chrisducote@Chriss-MacBook-Air.local>
2020-01-13 11:59:35 +0100

    drawing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+
+  componentDidMount = () => {
+    window.addEventListener('load', () => {
+      const canvas = document.querySelector('#canvas');
+      const ctx = canvas.getContext('2d')
+
+      //Resizing
+      canvas.height = window.innerHeight;
+      canvas.width = window.innerWidth;
+      //variables
+      let painting = false;
+
+      let startPosition = () => {
+        painting = true
+      };
+
+      let finishedPosition = () => {
+        painting = false
+      };
+
+      let draw = (e) => {
+        if(!painting) return;
+        ctx.lineWidth = 10;
+        ctx.lineCap = 'round'
+        ctx.lineTo(e.clientX, e.clientY)
+        ctx.stroke()
+      }
+
+      // Event Listeners
+      canvas.addEventListener('mousedown', startPosition)
+      canvas.addEventListener('mouseup', finishedPosition)
+      canvas.addEventListener('mousemove', draw)
+
+    })
+  }
+render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <canvas id='canvas'>
+      </canvas>
     </div>
   );
+ }
 }
 
 export default App;
